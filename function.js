df8691949a53e691939c85bca1b10d773460943e
Kim_Arthem <sonkimarthem@gmail.com>
2024-10-31 15:55:31 +0800

    Calculator

---
+++ function.js
@@ -0,0 +1,8 @@
+
+
+const btn = document.getElementById("btn");
+const ShowVal = document.getElementById("value");
+let calculate = (value)=>{
+    ShowVal.textContent+=value;
+    console.log(value)
+}
